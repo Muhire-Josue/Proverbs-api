ef6f970ced83d19b8e9bbe1046cdf709fd71ce10
Muhire-Josue <rutayisire.josue09@gmail.com>
2021-01-11 12:38:33 +0200

    validate commentId

--- server/routes/comment.routes.js
+++ server/routes/comment.routes.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import CommentController from '../controllers/comment.controller';
+import { checkCommentExist, validateCommentId } from '../middlewares/checkCommentId.middleware';
 
 const route = express.Router();
 const { CommentProverb, editComment, removeComment } = CommentController;
 
 route.post('/proverb/comment', CommentProverb);
-route.put('/proverb/comment/edit/:commentId', editComment);
-route.delete('/proverb/comment/delete/:commentId', removeComment);
+route.put('/proverb/comment/edit/:commentId', [validateCommentId, checkCommentExist], editComment);
+route.delete('/proverb/comment/delete/:commentId', [validateCommentId, checkCommentExist], removeComment);
 
 export default route;
